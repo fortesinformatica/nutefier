c2e8130460b2721d03ed11c74bb328b5bdf399e5
Rodrigo Maia <rodrigomb@gmail.com>
2016-05-25 16:48:05 -0300

    despedida do Yurizão jaja

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 var http = require('http').Server(),
+		https = require('https'),
+		fs = require('fs'),
 		io = require('socket.io')(http);
 
 http.listen(80, function(){
 	console.log("Server configured for: " + (global.process.env.NODE_ENV || 'development') + " environment.");
-	console.log("Server running on :9999");
+	console.log("Server running on :80");
+  console.log(new Date);
+});
+
+var options = {
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem')
+};
+
+https.createServer(options, app).listen(443, function(){
+	console.log("Server running on 443");
   console.log(new Date);
 });
 

db5d9a4ab138f41545b621653b04b6157e6103c3
Rodrigo Maia <rodrigomb@gmail.com>
2016-05-25 16:42:13 -0300

    desespero

--- app.js
+++ app.js
@@ -1,35 +1,9 @@
-var express = require('express');
-var https = require('https');
-var http = require('http');
-var fs = require('fs');
-var io = require('socket.io')(http.Server());
-// This line is from the Node.js HTTPS documentation.
-var options = {
-  key: fs.readFileSync('key.pem'),
-  cert: fs.readFileSync('cert.pem')
-};
+var http = require('http').Server(),
+		io = require('socket.io')(http);
 
-// var port = 9999;
-//
-// if (global.process.env.NODE_ENV != 'production'){
-// 	port = 9998;
-// }
-
-// Create a service (the app object is just a callback).
-var app = express();
-
-app.get('/', function (req, res) {
-  res.send('NUTEfier on air');
-});
-
-// Create an HTTP service.
-http.createServer(app).listen(80, function(){
-	console.log("Server running on 80");
-  console.log(new Date);
-});
-// Create an HTTPS service identical to the HTTP service.
-https.createServer(options, app).listen(443, function(){
-	console.log("Server running on 443");
+http.listen(80, function(){
+	console.log("Server configured for: " + (global.process.env.NODE_ENV || 'development') + " environment.");
+	console.log("Server running on :9999");
   console.log(new Date);
 });
 
@@ -38,7 +12,7 @@ io.on('connection', function(socket){
 
 	socket.on('notificar', function(grupo_id){
 		console.log('notify @', grupo_id);
-		socket.broadcast.emit('novidade', grupo_id);
+		socket.broadcast.emit('notif', grupo_id);
 	});
 
   socket.on('disconnect', function(){
